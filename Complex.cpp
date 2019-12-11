963afd5c3e12c100fdfed1c6cd61f5b59ca45eff
dontforgetmy <51456509+dontforgetmy@users.noreply.github.com>
2019-12-11 23:13:20 +0700

    Add files via upload

---
+++ Complex.cpp
@@ -0,0 +1,26 @@
+﻿// Complex.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Complex.h"
+#include <iomanip>
+#include <string.h>
+#include <string>
+int main()
+{
+	MyComplex a(1, -1), b(0, 0), c, d;
+	cout << "Enter c" << endl;
+	cin >> c;
+	cout << "Enter d" << endl;
+	cin >> d;
+	double q = 5;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	b = a;
+	cout << "b=a" << b << endl;
+	cout << "c+d=" << *&(c + d) << endl;
+	cout << "c-q=" << *&(c - q) << endl;
+	cout << "c-d=" << *&(c - d) << endl;
+	cout << "c*d=" << *&(d * c) << endl;
+}
+
